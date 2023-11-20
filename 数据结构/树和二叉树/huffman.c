7cb0423d348811305f4d7d33453598dd8fe0cf2d
to-undefined <q1510017673@outlook.com>
2023-11-20 22:20:43 +0800

    Nov 20

--- 数据结构/树和二叉树/huffman.c
+++ 数据结构/树和二叉树/huffman.c
@@ -1,1 +1,17 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct huffman
+{
+    int weight;
+    int parent, lchild, rchild;
+} HTNode, *HuffmanTree;
+//构造哈夫曼树
+void CreateHuffmanTree(HuffmanTree *HT, int n);
+int main(int argc, char *argv[])
+{
+    
+}
+void CreateHuffmanTree(HuffmanTree *HT, int n)
+{
+    
+}
