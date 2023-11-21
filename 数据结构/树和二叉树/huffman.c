e410d711b5e5c5e7d0ac24fdf3a8993402c6d06e
to-undefined <q1510017673@outlook.com>
2023-11-21 21:09:20 +0800

    Nov 21

--- 数据结构/树和二叉树/huffman.c
+++ 数据结构/树和二叉树/huffman.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 }
 void CreateHuffmanTree(HuffmanTree *HT, int n)
 {
-    //初始化
+    // 初始化
     if (n <= 1)
         return;
     int m = 2 * n - 1;
@@ -25,16 +25,16 @@ void CreateHuffmanTree(HuffmanTree *HT, int n)
     }
     for (int i = 1; i <= n; ++i)
         scanf("%d", &(*HT)[i].weight);
-    //创建哈夫曼树
-    for(int i =n+1;i<=m;++i)
+    // 创建哈夫曼树
+    for (int i = n + 1; i <= m; ++i)
     {
-        //通过n-1次的选择、删除、合并来创建哈夫曼树
+        // 通过n-1次的选择、删除、合并来创建哈夫曼树
         int s1, s2;
-        select(*HT, i-1, s1, s2);
-        (*HT)[s1].parent=i;
-        (*HT)[s2].parent=i;
-        (*HT)[i].lchild=s1;
-        (*HT)[i].rchild=s2;
-        (*HT)[i].weight=(*HT)[s1].weight+(*HT)[s2].weight;
-    }   
+        select(*HT, i - 1, s1, s2);
+        (*HT)[s1].parent = i;
+        (*HT)[s2].parent = i;
+        (*HT)[i].lchild = s1;
+        (*HT)[i].rchild = s2;
+        (*HT)[i].weight = (*HT)[s1].weight + (*HT)[s2].weight;
+    }
 }
