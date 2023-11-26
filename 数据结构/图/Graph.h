47b6c74b8412752cc9d08b86bec458d68189d1f4
to-undefined <q1510017673@outlook.com>
2023-11-27 00:05:49 +0800

    Nov 27

---
+++ 数据结构/图/Graph.h
@@ -0,0 +1,9 @@
+#define MaxInt 32767
+#define MVNum 100
+typedef char VerTexType;
+typedef int ArcType;
+typedef struct{
+	VerTexType vexs[MVNum];
+	ArcType arcs[MVNum][MVNum];
+	int vexnum, arcnum;
+}AMGraph;
--- 数据结构/栈和队列/que.c
+++ 数据结构/栈和队列/que.c
@@ -2,31 +2,32 @@
 void createQueue(Queue *que, int n);
 void printQueue(Queue *que);
 int main(int argc, char *argv[]){
-    Queue queue;
-    queue_init(&queue);
-    createQueue(&queue, 10);
-    printQueue(&queue);
-    queue_in(&queue, 10);
-    printQueue(&queue);
-    int value = queue_out(&queue);
-    printf("对首元素: %d\n", value);
-    printQueue(&queue);
-  
+	Queue queue;
+	queue_init(&queue);
+	createQueue(&queue, 10);
+	printQueue(&queue);
+	queue_in(&queue, 10);
+	printQueue(&queue);
+	int value = queue_out(&queue);
+	printf("对首元素: %d\n", value);
+	printQueue(&queue);
+
 }
 void createQueue(Queue *que, int n){
-    printf("输入%d个数据: ", n);
-    for(int i = 0;i < n; i++){
-        int value;
-        scanf("%d", &value);
-        queue_in(que,value);
-    }
+	printf("输入%d个数据: ", n);
+	for(int i = 0;i < n; i++){
+		int value;
+		scanf("%d", &value);
+		queue_in(que,value);
+	}
 }
 void printQueue(Queue *que){
-    int begin = que->front, end = que->rear;
-    printf("打印数据: ");
-    while (begin != end) {
-    printf("%d ", que->base[begin]);
-    begin = (begin + 1) % MAXSIZE;
-    }
-    printf("\n");
+	int begin = que->front, end = que->rear;
+	printf("打印数据: ");
+	while (begin != end) {
+		printf("%d ", que->base[begin]);
+		begin = (begin + 1) % MAXSIZE;
+	}
+	printf("\n");
 }
+
