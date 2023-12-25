e2471978c7e293090f13a0744842cf0b001442c8
to-undefined <q1510017673@outlook.com>
2023-12-25 18:14:31 +0800

    Dec 25

--- 课程设计/main.cpp
+++ 课程设计/main.cpp
@@ -5,8 +5,8 @@ int main() {
 	Map map;
 	cout << "请输入选项" << endl;
 	cout << "1 存储" << endl;
-	cout << "2 各个城市最短距离" << endl;
-	cout << "3 两个城市最短距离" << endl;
+	cout << "2 各个城市最短路径" << endl;
+	cout << "3 两个城市最短路径" << endl;
 	cout << "输入选项" << endl;
 	int op;
 	string begin;
@@ -32,5 +32,5 @@ int main() {
 		default:
 			break;
 	}
-	map.fread();
+	return 0;
 }
