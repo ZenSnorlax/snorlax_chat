a11f57f9ec90e0a2cefc6ace79871fc24f413281
to-undefined <q1510017673@outlook.com>
2024-05-17 22:07:42 +0800

    24 Apr

--- 数据结构/图/traverse_dfs.c
+++ 数据结构/图/traverse_dfs.c
@@ -1,25 +1,24 @@
 #include "Graph.h"
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
 bool visited[MVNum];
 void dfs_traverse(const AMGraph *G, const int v) {
-	printf("访问%c\n", G->vexs[v]);
-	visited[v] = true;
-	for (int i = 0; i < G->vexnum; ++i)
-		if(!visited[i] && G->arcs[v][i] < MaxInt)
-			dfs_traverse(G, i);
-
+    printf("访问%c\n", G->vexs[v]);
+    visited[v] = true;
+    for (int i = 0; i < G->vexnum; ++i)
+        if (!visited[i] && G->arcs[v][i] < MaxInt)
+            dfs_traverse(G, i);
 }
-void DFS(AMGraph *G){
-	for (int v = 0; v < G->vexnum; ++v)
-		visited[v] = false;
-	for (int v = 0; v < G->vexnum; ++v)
-		if(!visited[v])
-			dfs_traverse(G, v);
+void DFS(AMGraph *G) {
+    for (int v = 0; v < G->vexnum; ++v)
+        visited[v] = false;
+    for (int v = 0; v < G->vexnum; ++v)
+        if (!visited[v])
+            dfs_traverse(G, v);
 }
 int main() {
-	AMGraph AG;
-	CreateUDN(&AG);
-	DFS(&AG);
-	return ;
+    AMGraph AG;
+    CreateUDN(&AG);
+    DFS(&AG);
+    return 0;
 }
