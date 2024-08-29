32d826b212e7e557c340f42174d4b00f2d21c1ae
to-undefined <q1510017673@outlook.com>
2024-08-29 12:35:19 +0800

    updata app.py

--- zensnorlax_blog/app.py
+++ zensnorlax_blog/app.py
@@ -12,18 +12,6 @@ def index():
 def archives():
     return "archives"
 
-
-@app.route("/2024/8/hello")
-def page_0():
-    return "index"
-
-
-@app.route("/2024/8/hekk")
-def page_1():
-    return "index"
-
-
-
-@app.route("/2024/8/j")
-def page_2():
+@app.route("/2024/8/你的敏感,就是你的天赋")
+def page_4():
     return "index"
