f36cbe56844b8de141b4048803c682a9643364bd
to-undefined <q1510017673@outlook.com>
2024-08-29 17:00:23 +0800

    add

--- zensnorlax_blog/app.py
+++ zensnorlax_blog/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -15,6 +15,5 @@ def archives():
 
 @app.route("/2024/8/29")
 def page_5():
-    # 使用 url_for 生成指向静态文件的 URL
-    file_url = url_for("static", filename="2024_8_29.html")
-    return f"<a href='{file_url}'>Open the file</a>"
+    # 渲染模板 '2024_8_29.html' 并返回其内容
+    return render_template("2024_8_29.html")
