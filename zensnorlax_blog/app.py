a8de4902213910f6559624a2b8010468975fb79e
to-undefined <q1510017673@outlook.com>
2024-08-29 12:08:57 +0800

    add

--- zensnorlax_blog/app.py
+++ zensnorlax_blog/app.py
@@ -11,8 +11,3 @@ def index():
 @app.route("/archives")
 def archives():
     return "archives"
-
-
-@app.route("/2024/8/inde")
-def index():
-    return "index"
--- zensnorlax_blog/create_route.py
+++ zensnorlax_blog/create_route.py
@@ -9,8 +9,11 @@ month = datetime.datetime.now().month
 # 获取post名称
 post = input("请输入post名称: ")
 
+# 自动根据日期生成函数路由名称
+route_name = f"{post}_{year}_{month}"
+
 route = f"""\n\n\n@app.route("/{year}/{month}/{post}")
-def index():
+def {route_name}():
     return "index"
 """
 with open("app.py", "a") as f:
