483eccb08ef52447e3b70afc66b873614f79866b
ZenSnorlax <q1510017673@outlook.com>
2024-09-23 21:55:29 +0800

    add timeout and error retries

--- conn_pool/conn_pool.hpp
+++ conn_pool/conn_pool.hpp
@@ -1,11 +1,13 @@
 #include <mysql-cppconn/mysqlx/xdevapi.h>
 
+#include <chrono>
 #include <condition_variable>
 #include <cstddef>
 #include <memory>
 #include <mutex>
 #include <queue>
 #include <string>
+#include <thread>
 
 #include "../snorlax_log.hpp"
 
@@ -24,17 +26,40 @@ class SQLConnPool {
         return conn_pool;
     }
 
-    std::unique_ptr<mysqlx::Session> getConnection() {
-        std::unique_lock<std::mutex> lock(mutex_);
-        cv_.wait(lock, [this] { return !queue_.empty(); });
-        auto session = std::move(queue_.front());
-        queue_.pop();
-
-        LOG(Level::DEBUG,
-            "thread ",
-            std::this_thread::get_id(),
-            " attain a connection");
-        return session;
+    std::unique_ptr<mysqlx::Session> getConnection(
+        std::chrono::milliseconds timeout, int max_retries = 3,
+        std::chrono::milliseconds retry_interval =
+            std::chrono::milliseconds(1000)) {
+        for (int attempt = 0; attempt <= max_retries; ++attempt) {
+            std::unique_lock<std::mutex> lock(mutex_);
+            if (cv_.wait_for(
+                    lock, timeout, [this] { return !queue_.empty(); })) {
+                auto session = std::move(queue_.front());
+                queue_.pop();
+                LOG(Level::DEBUG,
+                    "thread ",
+                    std::this_thread::get_id(),
+                    " get a connection on attemp ",
+                    attempt + 1);
+                return session;
+            } else {
+                LOG(Level::WARNING,
+                    "thread ",
+                    std::this_thread::get_id(),
+                    " failed to get a connection on attempt ",
+                    attempt + 1,
+                    " within timeout");
+                if (attempt == max_retries) {
+                    LOG(Level::ERROR,
+                        "thread ",
+                        std::this_thread::get_id(),
+                        " exhausted retries. Failed to get a connection.");
+                    return nullptr;
+                }
+            }
+            std::this_thread::sleep_for(retry_interval);
+        }
+        return nullptr;
     }
 
     void releaseConnection(std::unique_ptr<mysqlx::Session> session) {
