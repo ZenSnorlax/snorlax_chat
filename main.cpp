6a8d3336d50b677d7ab743290fdf7735b1c2b6af
ZenSnorlax <q1510017673@outlook.com>
2024-09-29 16:14:34 +0800

    updata

--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "MySQL Username: " << MYSQL_USERNAME << std::endl;
-    std::cout << "MySQL Password: " << MYSQL_PASSWORD << std::endl;
-    std::cout << "MySQL Database: " << MYSQL_DATABASE << std::endl;
-    return 0;
-}
