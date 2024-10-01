5c3d6884f6380efa76f0ca8f1208cb4dad2113b2
ZenSnorlax <q1510017673@outlook.com>
2024-10-01 22:09:42 +0800

    modfiy

--- script/init.sql
+++ script/init.sql
@@ -5,45 +5,45 @@ CREATE TABLE users (
     id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(50) UNIQUE NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    email VARCHAR(100) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     last_login TIMESTAMP,
     status ENUM('active', 'inactive') DEFAULT 'active'
-)
+);
 
--- if chatrooms table exists, drop it
-DROP TABLE IF EXISTS chatrooms;
--- create chatrooms table
+-- if chat_rooms table exists, drop it
+DROP TABLE IF EXISTS chat_rooms;
+-- create chat_rooms table
 CREATE TABLE chat_rooms (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    room_name VARCHAR(100) UNIQUE NOT NULL
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    room_name VARCHAR(100) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     created_by INT NOT NULL,
     FOREIGN KEY (created_by) REFERENCES users(id)
-)
+);
 
---if messages table exists, drop it
+-- if messages table exists, drop it
 DROP TABLE IF EXISTS messages;
 -- create messages table
 CREATE TABLE messages (
     id INT PRIMARY KEY AUTO_INCREMENT,
-   room_id INT NOT NULL,
-   user_id INT NOT NULL,
-   content TEXT NOT NULL,
-   timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-   FOREIGN KEY (room_id) REFERENCES chat_rooms(id),
-   FOREIGN KEY (user_id) REFERENCES users(id)
-)
+    room_id INT NOT NULL,
+    user_id INT NOT NULL,
+    content TEXT NOT NULL,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (room_id) REFERENCES chat_rooms(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
 
---if user_room_relations table exists, drop it
+-- if user_room_relations table exists, drop it
 DROP TABLE IF EXISTS user_room_relations;
 -- create user_room_relations table
 CREATE TABLE user_room_relations (
     user_id INT NOT NULL,
     room_id INT NOT NULL,
     joined_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    role  ENUM('admin', 'member') DEFAULT 'member',
+    role ENUM('admin', 'member') DEFAULT 'member',
     PRIMARY KEY (user_id, room_id),
     FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (room_id) REFERENCES chat_rooms(id)
-)
+);
