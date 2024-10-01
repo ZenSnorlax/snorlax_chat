d47f25173b9fa74c0a36cd33111fe2c343ed1c03
ZenSnorlax <q1510017673@outlook.com>
2024-10-01 22:34:49 +0800

    add and updata

---
+++ script/init.py
@@ -0,0 +1,17 @@
+import subprocess
+
+# define the MySQL connection details
+user = 'abs'
+password = '1510017673'
+database = 'snorlax_chat'
+sql_file = 'setup.sql'
+
+# construct the command
+command = f'mysql -u {user} -p{password} {database} < {sql_file}'
+
+
+try:
+    subprocess.run(command, shell=True, check=True)
+    print("SQL file executed successfully.")
+except subprocess.CalledProcessError as e:
+    print(f"Error executing SQL file: {e}")
